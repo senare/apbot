e21d53c0832a458d7ec51189ce36e12f0b1bc2f8
senare <senare@gmail.com>
2014-11-28 00:25:03 +0100

    fix

--- src/main/java/ao/apbot/SessionHandler.java
+++ src/main/java/ao/apbot/SessionHandler.java
@@ -23,80 +23,83 @@ import ao.apbot.pkg.auth.PingPacket;
 
 public class SessionHandler extends IoHandlerAdapter {
 
-	private final static Logger log = LoggerFactory.getLogger(SessionHandler.class);
+    private final static Logger log = LoggerFactory.getLogger(SessionHandler.class);
 
-	private String password;
-	private String username;
-	private String handle;
+    private String password;
+    private String username;
+    private String handle;
 
-	private KieContainer kc;
+    private KieContainer kc;
 
-	public SessionHandler(String handle, String username, String password) {
-		this.handle = handle;
-		this.username = username;
-		this.password = password;
+    public SessionHandler(String handle, String username, String password) {
+        this.handle = handle;
+        this.username = username;
+        this.password = password;
 
-		KieServices ks = KieServices.Factory.get();
-		this.kc = ks.getKieClasspathContainer();
-	}
+        KieServices ks = KieServices.Factory.get();
+        this.kc = ks.getKieClasspathContainer();
+    }
 
-	@Override
-	public void messageReceived(IoSession session, Object message) {
-		log.info("{} received {}", handle, message);
+    @Override
+    public void messageReceived(IoSession session, Object message) {
+        log.info("{} received {}", handle, message);
 
-		if (message instanceof Fact) {
-			Fact pkg = (Fact) message;
-			switch (pkg.getType()) {
-			case PingPacket.TYPE:
-				session.write(new PingPacket(((PingPacket) pkg).getMessage()));
-				break;
-			case LoginSeedPacket.TYPE:
-				session.write(new LoginRequestPacket((LoginSeedPacket) pkg, username, password));
-				break;
-			case CharacterListPacket.TYPE:
-				session.write(new LoginSelectPacket((CharacterListPacket) pkg, handle));
-				break;
-			case LoginOkPacket.TYPE:
-				log.info("{} has logged on", handle);
-				break;
-			case LoginErrorPacket.TYPE:
-				log.info("{} failed to logon", handle);
-				break;
-			default:
-				KieSession ksession = kc.newKieSession("APbotSession");
+        if (message instanceof Fact) {
+            Fact pkg = (Fact) message;
+            switch (pkg.getType()) {
+            case PingPacket.TYPE:
+                session.write(new PingPacket(((PingPacket) pkg).getMessage()));
+                break;
+            case LoginSeedPacket.TYPE:
+                session.write(new LoginRequestPacket((LoginSeedPacket) pkg, username, password));
+                break;
+            case CharacterListPacket.TYPE:
+                session.write(new LoginSelectPacket((CharacterListPacket) pkg, handle));
+                break;
+            case LoginOkPacket.TYPE:
+                log.info("{} has logged on", handle);
+                break;
+            case LoginErrorPacket.TYPE:
+                log.info("{} failed to logon", handle);
+                break;
+            default:
+                KieSession ksession = kc.newKieSession("APbotSession");
 
-				// map for all bot's, or something ??
-				ksession.setGlobal("session", session);
+                // map for all bot's, or something ??
+                ksession.setGlobal("session", session);
 
-				ksession.insert(new TimeFact(Calendar.getInstance()));
-				ksession.insert(pkg);
-				ksession.fireAllRules();
-				ksession.dispose();
-			}
-		} else {
-			session.close(true);
-		}
-	}
+                Calendar now = Calendar.getInstance();
+                now.set(Calendar.DAY_OF_MONTH, 24);
+                now.set(Calendar.MONTH,Calendar.DECEMBER);
+                ksession.insert(new TimeFact(now));
+                ksession.insert(pkg);
+                ksession.fireAllRules();
+                ksession.dispose();
+            }
+        } else {
+            session.close(true);
+        }
+    }
 
-	@Override
-	public void sessionOpened(IoSession session) {
-		log.info("{} connect", handle);
-	}
+    @Override
+    public void sessionOpened(IoSession session) {
+        log.info("{} connect", handle);
+    }
 
-	@Override
-	public void messageSent(IoSession session, Object message) throws Exception {
-		log.info("{} sent {}", handle, message);
-	};
+    @Override
+    public void messageSent(IoSession session, Object message) throws Exception {
+        log.info("{} sent {}", handle, message);
+    };
 
-	@Override
-	public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
-		log.info("{} idle {}", handle, status);
-		session.write(new PingPacket("Java AOChat API ping"));
-	}
+    @Override
+    public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
+        log.info("{} idle {}", handle, status);
+        session.write(new PingPacket("Java AOChat API ping"));
+    }
 
-	@Override
-	public void exceptionCaught(IoSession session, Throwable cause) {
-		log.error("Client caught exception ", cause);
-		session.close(true);
-	}
+    @Override
+    public void exceptionCaught(IoSession session, Throwable cause) {
+        log.error("Client caught exception ", cause);
+        session.close(true);
+    }
 }
--- src/main/java/ao/apbot/codec/TimeFact.java
+++ src/main/java/ao/apbot/codec/TimeFact.java
@@ -2,38 +2,37 @@ package ao.apbot.codec;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 public class TimeFact {
 
-	private SimpleDateFormat format = new SimpleDateFormat("YYYYY-MM-DD T hh:mm:ss");
-	private Calendar rk = null;
+    // http://xkcd.com/1179/
+    private final static SimpleDateFormat format = new SimpleDateFormat("YYYYY-MM-dd 'T' HH:mm:ss");
+    static {
+        format.setTimeZone(TimeZone.getTimeZone("UTZ"));
+    }
 
-	public TimeFact(Calendar now) {
-		this.rk = now;
-		rk.add(Calendar.YEAR, 27474);
-	}
+    private Calendar rk = null;
+    private String greet = "";
+    
+    public TimeFact(Calendar now) {
+        this.rk = now;
+        rk.add(Calendar.YEAR, 27474);
+    }
 
-	public int getMonth() {
-		return rk.get(Calendar.MONTH);
-	}
+    public int getMonth() {
+        return rk.get(Calendar.MONTH);
+    }
 
-	public int getDay() {
-		return rk.get(Calendar.DAY_OF_MONTH);
-	}
+    public int getDay() {
+        return rk.get(Calendar.DAY_OF_MONTH);
+    }
 
-	public String toString() {
-		return String.format("It is currently %s Rubi-Ka Universal Time. %s", format.format(rk.getTime()), greet());
-	}
+    public String toString() {
+        return String.format("It is currently %s Rubi-Ka Universal Time. %s", format.format(rk.getTime()), greet);
+    }
 
-	private String greet() {
-		String greet = "";
-		if (rk.get(Calendar.MONTH) == Calendar.DECEMBER && rk.get(Calendar.DAY_OF_MONTH) == 24) {
-			greet += "\n ##red##Merry##end## ##lime##Christmas##end## ##red##from##end## ##lime##Santa##end## ##red##Leet !!##end##";
-		} else if (rk.get(Calendar.MONTH) == Calendar.OCTOBER && rk.get(Calendar.DAY_OF_MONTH) == 31) {
-			greet += "\n ##darkorange##Happy Halloween from Uncle Pumpkin-head !!##end##";
-		} else if (rk.get(Calendar.MONTH) == Calendar.JUNE && rk.get(Calendar.DAY_OF_MONTH) == 27) {
-			greet += "\n Happy Birthday Rubi Ka";
-		}
-		return greet;
-	}
+    public void setGreating(String greating){
+        this.greet = greating;
+    }
 }
